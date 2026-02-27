f038d1a56b28f8c1f0c35aaa1def8e1b4d8efe88
Dizane <dbhutia0517@gmail.com>
2026-02-27 09:47:40 +0530

    Create ZeroArgConstructorDemo.java

---
+++ ZeroArgConstructorDemo.java
@@ -0,0 +1,11 @@
+class Student {
+    Student() {
+        System.out.println("This is a 0-argument constructor");
+    }
+}
+
+public class ZeroArgConstructorDemo {
+    public static void main(String[] args) {
+        Student s = new Student();
+    }
+}
